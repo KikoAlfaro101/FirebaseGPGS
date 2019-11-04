6c0c03e806911229881128e7506b49bf288467c0
KikoAlfaro101 <kiko@centounopercento.com>
2019-11-04 18:27:27 +0100

    Anonymous Sign in and Basics of Database working well

---
+++ Assets/Scripts/AnonymusManager.cs
@@ -0,0 +1,90 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnonymusManager : MonoBehaviour
+{
+
+    public Image stateImg;
+    public Sprite successImg, failImg;
+    public Text fbUserText;
+
+    //[Space]
+    //public DatabaseManager dbManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                //   app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+                Debug.Log("TUTTO OK");
+                fbUserText.text = "Dependency OK";
+
+            }
+            else
+            {
+                fbUserText.text = "Dependency FAILED";
+
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+
+
+            }
+
+
+        });
+    }
+
+    void Update()
+    {
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        if (user != null)
+        {
+            stateImg.sprite = successImg;
+        }
+        else
+        {
+            stateImg.sprite = failImg;
+        }
+    }
+
+    public void AnonymousSignIn()
+    {
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+
+        auth.SignInAnonymouslyAsync().ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInAnonymouslyAsync was canceled.");
+                stateImg.sprite = failImg;
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInAnonymouslyAsync encountered an error: " + task.Exception);
+                stateImg.sprite = failImg;
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+
+            fbUserText.text = newUser.UserId;
+        });
+    }
+
+}
---
+++ Assets/Scripts/DatabaseManager.cs
@@ -0,0 +1,64 @@
+﻿using Firebase;
+using Firebase.Database;
+using Firebase.Unity.Editor;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class User
+{
+    public string username;
+    public string email;
+
+    public User()
+    {
+    }
+
+    public User(string username, string email)
+    {
+        this.username = username;
+        this.email = email;
+    }
+}
+
+public class DatabaseManager : MonoBehaviour
+{
+    // References
+    public InputField userInputfield;
+    public InputField emailInputfield;
+
+    public int lastUserID = 0; // Allowed ID
+
+    // Firebase
+    DatabaseReference dbReference;
+
+    public void SetUpDatabase()
+    {
+        // Set up the Editor before calling into the realtime database.
+        FirebaseApp.DefaultInstance.SetEditorDatabaseUrl("https://signuptest-kikoalfaro.firebaseio.com/");
+
+        // Get the root reference location of the database.
+        dbReference = FirebaseDatabase.DefaultInstance.RootReference;
+    }
+
+    public void OnStoreUserPressed()
+    {
+        if(dbReference == null) SetUpDatabase(); // Must to research a callback-based way to do this...
+
+        writeNewUser(lastUserID.ToString(), userInputfield.text, emailInputfield.text);
+        lastUserID++;
+    }
+
+    private void writeNewUser(string userId, string name, string email)
+    {
+        User user = new User(name, email);
+        string json = JsonUtility.ToJson(user);
+
+        dbReference.Child("users").Child(userId).SetRawJsonValueAsync(json);
+
+        // If user exists but it wants to update its data (e.g. "username"), it's possible to access by:
+        // mDatabaseRef.Child("users").Child(userId).Child("username").SetValueAsync(name);
+
+    }
+}
