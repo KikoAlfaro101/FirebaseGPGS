04d67140b38194e163384397a3693ddbe7c3bbcd
KikoAlfaro101 <kiko@centounopercento.com>
2019-11-04 11:14:28 +0100

    1st commit

---
+++ Assets/Scripts/FirebaseManager.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FirebaseManager : MonoBehaviour
+{
+    public Sprite successImg, failImg;
+    public Image debugFBImg;
+
+    public Text fbUserText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Firebase.FirebaseApp.CheckAndFixDependenciesAsync().ContinueWith(task => {
+            var dependencyStatus = task.Result;
+            if (dependencyStatus == Firebase.DependencyStatus.Available)
+            {
+                // Create and hold a reference to your FirebaseApp,
+                // where app is a Firebase.FirebaseApp property of your application class.
+                //   app = Firebase.FirebaseApp.DefaultInstance;
+
+                // Set a flag here to indicate whether Firebase is ready to use by your app.
+                Debug.Log("TUTTO OK");
+
+                fbUserText.text = "Dependency OK";
+            }
+            else
+            {
+                UnityEngine.Debug.LogError(System.String.Format(
+                  "Could not resolve all Firebase dependencies: {0}", dependencyStatus));
+                // Firebase Unity SDK is not safe to use here.
+
+                fbUserText.text = "Dependency FAILED";
+
+            }
+        });
+    }
+
+    internal void SignIn(string authCode)
+    {
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        Firebase.Auth.Credential credential =
+            Firebase.Auth.PlayGamesAuthProvider.GetCredential(authCode);
+        auth.SignInWithCredentialAsync(credential).ContinueWith(task => {
+            if (task.IsCanceled)
+            {
+                Debug.LogError("SignInWithCredentialAsync was canceled.");
+                fbUserText.text = "SignInWithCredentialAsync was canceled.";
+                return;
+            }
+            if (task.IsFaulted)
+            {
+                Debug.LogError("SignInWithCredentialAsync encountered an error: " + task.Exception);
+                fbUserText.text = "SignInWithCredentialAsync encountered an error: " + task.Exception;
+                return;
+            }
+
+            Firebase.Auth.FirebaseUser newUser = task.Result;
+            Debug.LogFormat("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+
+            fbUserText.text = String.Format("User signed in successfully: {0} ({1})",
+                newUser.DisplayName, newUser.UserId);
+        });
+    }
+
+    internal void SignOut()
+    {
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+
+        auth.SignOut();
+        user.DeleteAsync();
+
+        fbUserText.text = "";
+    }
+
+    public Firebase.Auth.FirebaseUser GetFirebaseUser()
+    {
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        if (user != null)
+        {
+            string playerName = user.DisplayName;
+
+            // The user's Id, unique to the Firebase project.
+            // Do NOT use this value to authenticate with your backend server, if you
+            // have one; use User.TokenAsync() instead.
+            string uid = user.UserId;
+        }
+
+        return user;
+    }
+
+    public void OnShowUserButtonPressed()
+    {
+        Firebase.Auth.FirebaseUser user = GetFirebaseUser();
+        fbUserText.text = user.DisplayName;
+    }
+
+    private void Update()
+    {
+        Firebase.Auth.FirebaseAuth auth = Firebase.Auth.FirebaseAuth.DefaultInstance;
+        Firebase.Auth.FirebaseUser user = auth.CurrentUser;
+        if (user != null)
+        {
+            debugFBImg.sprite = successImg;
+        }
+        else
+        {
+            debugFBImg.sprite = failImg;
+        }
+    }
+}
---
+++ Assets/Scripts/GPGSManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using GooglePlayGames;
+using GooglePlayGames.BasicApi;
+using UnityEngine.SocialPlatforms;
+using System.Threading.Tasks;
+using UnityEngine.UI;
+using System;
+
+public class GPGSManager : MonoBehaviour
+{
+
+    public Sprite successImg, failImg;
+    public Image debugGPGSImg;
+    public FirebaseManager firebaseManager;
+
+
+    private string authCode; // Auth code from Play Games services --> It will be used as a Firebase credential in order to authenticate the player!
+    //public Text authStatus;
+
+    void Start()
+    {
+        InitGPGS();
+    }
+
+    private void InitGPGS()
+    {
+        Debug.Log("INIT GPGS...");
+
+        PlayGamesClientConfiguration config = new PlayGamesClientConfiguration.Builder()
+    .RequestServerAuthCode(false /* Don't force refresh */)
+    .Build();
+
+        PlayGamesPlatform.DebugLogEnabled = true; // Enable debugging output
+
+        PlayGamesPlatform.InitializeInstance(config);
+        PlayGamesPlatform.Activate();
+
+        //PlayGamesPlatform.Instance.Authenticate(SignInCallback, true);
+
+        GoogleSignin();
+    }
+
+    private void GoogleSignin()
+    {
+        if (!Social.localUser.authenticated)
+        {
+            Social.localUser.Authenticate(success => {
+                if (success)
+                {
+                    Debug.Log("success");
+                }
+                else
+                {
+                    Debug.Log("fail...");
+                }
+            });
+        }
+    }
+
+    public void OnSignInButtonPressed()
+    {
+        GoogleSignin();
+    }
+
+    public void OnSignOutButtonPressed()
+    {
+        PlayGamesPlatform.Instance.SignOut();
+    }
+
+    public void OnFirebaseSignInButtonPressed()
+    {
+        authCode = PlayGamesPlatform.Instance.GetServerAuthCode();
+        firebaseManager.SignIn(authCode);
+    }
+
+    public void OnFirebaseSignOutButtonPressed()
+    {
+        firebaseManager.SignOut();
+    }
+
+    private void Update()
+    {
+        if (Social.localUser.authenticated)
+        {
+            debugGPGSImg.sprite = successImg;
+        }
+        else
+        {
+            debugGPGSImg.sprite = failImg;
+        }
+    }
+}
